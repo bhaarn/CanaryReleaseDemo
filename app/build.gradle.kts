95d4d98de110598467212c077f0b42442a2ab31e
bhaarn <92562462+bhaarneval@users.noreply.github.com>
2024-01-07 15:05:41 +0530

    Changed version number to 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.padhuga.canaryreleasedemo"
         minSdk = 34
         targetSdk = 34
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
