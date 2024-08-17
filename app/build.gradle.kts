ab132b4a941e8f39982695aa28691781c953a322
Bhaarn <bharani.mars@gmail.com>
2024-08-17 15:23:23 +0530

    Update build.gradle.kts version to 5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.padhuga.canaryreleasedemo"
         minSdk = 34
         targetSdk = 34
-        versionCode = 4
-        versionName = "4.0"
+        versionCode = 5
+        versionName = "5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
